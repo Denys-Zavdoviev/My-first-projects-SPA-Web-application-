4b302cdc5e9e4670a7c8752f6e5523f81764e542
Zavdoviev Denys <denys.zavdoviev@student.sumdu.edu.ua>
2025-10-31 16:43:13 +0200

    Реалізація Хедера Футера 1

--- My-first-project/src/app/app.ts
+++ My-first-project/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Layout} from './layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Layout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('My-first-project');
 }
+
---
+++ My-first-project/src/app/footer/footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class Footer {
+  text_footer = 'Підвал сторінки'
+}
