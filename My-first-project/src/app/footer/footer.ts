ff6ab3e512dd6b401698de80108091cf6ec21174
Zavdoviev Denys <denys.zavdoviev@student.sumdu.edu.ua>
2025-11-06 20:59:30 +0200

    Обнова1 ПР2

--- My-first-project/src/app/footer/footer.ts
+++ My-first-project/src/app/footer/footer.ts
@@ -10,7 +10,7 @@ import {NgForOf} from '@angular/common';
 export class Footer {
   @Input() footerItems: string[] = [];
 
-  text_footer = '© 2025. Виконано в начальних цілях';
+  text_footer = '© 2025. Виконано в навчальних цілях';
   title_link =  "Зворотний зв'язок з нами =)";
   // list_feedback: string[] = ["Розділ 1", "Розділ 2", "Розділ 3"];
 }
